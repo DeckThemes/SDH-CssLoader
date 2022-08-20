7e45b59352acf101a5873d0f3082d2a69afa9e0c
joamjoamjoam <trentcalllan@yahoo.com>
2022-08-20 12:47:43 -0700

    Create Symlink in _main Remove redundant exists check for symlink Return a Result in create_symlink

--- main.py
+++ main.py
@@ -14,10 +14,6 @@ Logger = getLogger("CSS_LOADER")
 def Log(text : str):
     Logger.info(text)
 
-async def create_symlink(src : str, dst : str):
-    if not os.path.exists(dst):
-        os.symlink(src, dst, True)
-
 def createDir(dirPath : str):
     if (path.exists(dirPath)):
         return
@@ -41,6 +37,16 @@ class Result:
 
     def to_dict(self):
         return {"success": self.success, "message": self.message}
+
+async def create_symlink(src : str, dst : str) -> Result:
+   
+    try:
+        if not os.path.exists(dst):
+            os.symlink(src, dst, True)
+    except Exception as e:
+        return Result(False, str(e))
+
+    return Result(True)
 
 class Inject:
     def __init__(self, cssPath : str, tabs : List[str], theme):
@@ -758,10 +764,6 @@ class Plugin:
 
         themesPath = "/home/deck/homebrew/themes"
         defaultThemesPath = "/home/deck/homebrew/plugins/SDH-CssLoader/themes"
-        symlinkPath = "/home/deck/.local/share/Steam/steamui/themes_custom"
-
-        if (not os.path.exists(symlinkPath)):
-            await create_symlink(themesPath, symlinkPath)
 
         if (not path.exists(themesPath)):
             createDir(themesPath)
@@ -804,6 +806,7 @@ class Plugin:
 
         self.themes = []
         Log("Initializing css loader...")
+        await create_symlink("/home/deck/homebrew/themes", "/home/deck/.local/share/Steam/steamui/themes_custom")
         self.remote = RemoteInstall(self)
         await self.remote.load()
 
