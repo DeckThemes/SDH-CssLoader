f2bd15d88ae1a83b07cc39928638f636002dcdf5
beebls <logantkordov@gmail.com>
2022-12-31 14:04:15 -0700

    properly update themes

--- src/theme-manager/UninstallThemePage.tsx
+++ src/theme-manager/UninstallThemePage.tsx
@@ -1,17 +1,19 @@
 import { PanelSectionRow, Focusable, DialogButton } from "decky-frontend-lib";
-import { useState, VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import { FaTrash } from "react-icons/fa";
 import { AiOutlineDownload } from "react-icons/ai";
 import * as python from "../python";
 
 import { useCssLoaderState } from "../state";
 import { Theme } from "../theme";
-import { PartialCSSThemeInfo } from "../apiTypes";
+import { FullCSSThemeInfo } from "../apiTypes";
 
 export const UninstallThemePage: VFC = () => {
-  const { localThemeList, setLocalThemeList, browseThemeList, apiUrl } = useCssLoaderState();
+  const { localThemeList, setLocalThemeList, apiUrl } = useCssLoaderState();
 
   const [isUninstalling, setUninstalling] = useState(false);
+
+  const [updateStatuses, setUpdateStatuses] = useState<[string, false | FullCSSThemeInfo][]>([]);
 
   function handleUninstall(listEntry: Theme) {
     setUninstalling(true);
@@ -23,8 +25,8 @@ export const UninstallThemePage: VFC = () => {
     });
   }
 
-  function updateTheme(remoteEntry: PartialCSSThemeInfo) {
-    if (remoteEntry.id) {
+  function updateTheme(remoteEntry: FullCSSThemeInfo | false) {
+    if (remoteEntry && remoteEntry?.id) {
       const id = remoteEntry.id;
       setUninstalling(true);
       python.resolve(python.downloadThemeFromUrl(id, apiUrl), () => {
@@ -36,23 +38,32 @@ export const UninstallThemePage: VFC = () => {
     }
   }
 
-  // This is a modified version of the checkIfThemeInstalled in the browser page, however it compares local themes to the remote ones instead of remote to local
-  // It also returns the remote entry, so that it's id can be referenced for downloads
-  function checkForUpdate(themeObj: Theme): [string, any] {
-    const filteredArr: PartialCSSThemeInfo[] = browseThemeList.items.filter(
-      (e: PartialCSSThemeInfo) =>
-        e.name === themeObj.data.name && e.specifiedAuthor === themeObj.data.author
-    );
-    if (filteredArr.length > 0) {
-      if (filteredArr[0].version === themeObj.data.version) {
-        return ["installed", filteredArr[0]];
-      } else {
-        return ["outdated", filteredArr[0]];
-      }
-    } else {
-      return ["uninstalled", false];
+  // This gets the update status of all installed themes by querying them all.
+  useEffect(() => {
+    if (localThemeList.length > 0) {
+      const promiseArr: Promise<FullCSSThemeInfo>[] = [];
+      localThemeList.forEach((e) => {
+        const promise = python.genericGET(`${apiUrl}/themes/${e.id}`);
+        promiseArr.push(promise);
+      });
+      Promise.all(promiseArr).then((themeArr) => {
+        let updateStatusArr: [string, false | FullCSSThemeInfo][] = [];
+        themeArr.forEach((data, i) => {
+          const localEntry = localThemeList[i];
+          if (data?.version) {
+            if (data.version === localEntry.data.version) {
+              updateStatusArr.push(["installed", data]);
+            } else {
+              updateStatusArr.push(["outdated", data]);
+            }
+          } else {
+            updateStatusArr.push(["uninstalled", false]);
+          }
+        });
+        setUpdateStatuses(updateStatusArr);
+      });
     }
-  }
+  }, [localThemeList]);
 
   if (localThemeList.filter((e) => !e.data.bundled).length === 0) {
     return (
@@ -66,70 +77,75 @@ export const UninstallThemePage: VFC = () => {
     <>
       <div>
         <div>
-          {localThemeList
-            .filter((e) => !e.data.bundled)
-            .map((e: Theme) => {
-              const [updateStatus, remoteEntry] = checkForUpdate(e);
-              return (
-                <PanelSectionRow>
-                  <div
+          {localThemeList.map((e: Theme, i) => {
+            let [updateStatus, remoteEntry]: [string, false | FullCSSThemeInfo] = [
+              "installed",
+              false,
+            ];
+            // It now needs to wait until the promises have resolves to get these values
+            if (updateStatuses[i]) {
+              [updateStatus, remoteEntry] = updateStatuses[i];
+            }
+            return (
+              <PanelSectionRow>
+                <div
+                  style={{
+                    display: "flex",
+                    // TODO: I think that this 96% can be deleted, have to check
+                    width: "96%",
+                  }}
+                >
+                  <span>{e.data.name}</span>
+                  <span
                     style={{
-                      display: "flex",
-                      // TODO: I think that this 96% can be deleted, have to check
-                      width: "96%",
+                      color: "#dcdedf55",
+                      marginLeft: "8px",
                     }}
                   >
-                    <span>{e.data.name}</span>
-                    <span
+                    {e.data.version}
+                  </span>
+                  <Focusable
+                    style={{
+                      display: "flex",
+                      marginLeft: "auto",
+                      minWidth: "60%",
+                      maxWidth: "60%",
+                    }}
+                  >
+                    {/* Update Button */}
+                    {updateStatus === "outdated" && (
+                      <>
+                        <DialogButton
+                          style={{
+                            marginRight: "8px",
+                            minWidth: "calc(50% - 8px)",
+                            maxWidth: "calc(50% - 8px)",
+                            filter:
+                              "invert(6%) sepia(90%) saturate(200%) hue-rotate(160deg) contrast(122%)",
+                          }}
+                          onClick={() => updateTheme(remoteEntry)}
+                          disabled={isUninstalling}
+                        >
+                          <AiOutlineDownload />
+                        </DialogButton>{" "}
+                      </>
+                    )}
+                    <DialogButton
                       style={{
-                        color: "#dcdedf55",
-                        marginLeft: "8px",
-                      }}
-                    >
-                      {e.data.version}
-                    </span>
-                    <Focusable
-                      style={{
-                        display: "flex",
+                        minWidth: "calc(50% - 8px)",
+                        maxWidth: "calc(50% - 8px)",
                         marginLeft: "auto",
-                        minWidth: "60%",
-                        maxWidth: "60%",
                       }}
+                      onClick={() => handleUninstall(e)}
+                      disabled={isUninstalling}
                     >
-                      {/* Update Button */}
-                      {updateStatus === "outdated" && (
-                        <>
-                          <DialogButton
-                            style={{
-                              marginRight: "8px",
-                              minWidth: "calc(50% - 8px)",
-                              maxWidth: "calc(50% - 8px)",
-                              filter:
-                                "invert(6%) sepia(90%) saturate(200%) hue-rotate(160deg) contrast(122%)",
-                            }}
-                            onClick={() => updateTheme(remoteEntry)}
-                            disabled={isUninstalling}
-                          >
-                            <AiOutlineDownload />
-                          </DialogButton>{" "}
-                        </>
-                      )}
-                      <DialogButton
-                        style={{
-                          minWidth: "calc(50% - 8px)",
-                          maxWidth: "calc(50% - 8px)",
-                          marginLeft: "auto",
-                        }}
-                        onClick={() => handleUninstall(e)}
-                        disabled={isUninstalling}
-                      >
-                        <FaTrash />
-                      </DialogButton>
-                    </Focusable>
-                  </div>
-                </PanelSectionRow>
-              );
-            })}
+                      <FaTrash />
+                    </DialogButton>
+                  </Focusable>
+                </div>
+              </PanelSectionRow>
+            );
+          })}
         </div>
       </div>
     </>
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,6 @@
 export class Theme {
   data: any;
+  id: string = "";
   checked: boolean = false;
   name: string = "";
   description: string = "";
@@ -9,6 +10,7 @@ export class Theme {
   // This init function is called by the setter function in GlobalState
   init() {
     this.name = this.data.name;
+    this.id = this.data.id;
     this.checked = this.data.enabled;
     this.dependencies = this.data.dependencies;
 
