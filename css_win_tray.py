78faea949bfac60cb58c2310122fc46fe4f9d38d
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-06-29 20:11:58 +0200

    add tray icon developer toggle

--- css_win_tray.py
+++ css_win_tray.py
@@ -1,9 +1,10 @@
-import pystray, asyncio, css_theme, css_utils, os
+import pystray, css_theme, css_utils, os
 from PIL import Image, ImageDraw
 
 ICON = None
 MAIN = None
 LOOP = None
+DEV_MODE_STATE = False
 
 def reset():
     LOOP.create_task(MAIN.reset(MAIN))
@@ -15,16 +16,26 @@ def open_theme_dir():
 def exit():
     LOOP.create_task(MAIN.exit(MAIN))
 
+def get_dev_mode_state(x) -> bool:
+    return DEV_MODE_STATE
+
+def toggle_dev_mode_state():
+    global DEV_MODE_STATE
+    DEV_MODE_STATE = not DEV_MODE_STATE
+    LOOP.create_task(MAIN.toggle_watch_state(MAIN, get_dev_mode_state(None)))
+
 def start_icon(main, loop):
-    global ICON, MAIN, LOOP
+    global ICON, MAIN, LOOP, DEV_MODE_STATE
     MAIN = main
     LOOP = loop
+    DEV_MODE_STATE = MAIN.observer != None
     ICON = pystray.Icon(
     'CSS Loader',
     title='CSS Loader',
     icon=Image.open(os.path.join(os.path.dirname(__file__), "assets", "paint-roller-solid.png")),
     menu=pystray.Menu(
         pystray.MenuItem(f"CSS Loader {css_theme.CSS_LOADER_VER}", action=None, enabled=False),
+        pystray.MenuItem("Developer Mode", toggle_dev_mode_state, checked=get_dev_mode_state),
         pystray.MenuItem("Open Themes Folder", open_theme_dir),
         pystray.MenuItem("Reload Themes", reset),
         pystray.MenuItem("Exit", exit)
