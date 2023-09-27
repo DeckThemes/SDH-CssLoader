862c968d8f944ad7043512013ec8f923fcf91ec4
Beebles <102569435+beebls@users.noreply.github.com>
2023-09-27 09:43:47 -0600

    Update PluginSettings.tsx

--- src/pages/settings/PluginSettings.tsx
+++ src/pages/settings/PluginSettings.tsx
@@ -2,7 +2,7 @@ import { Focusable, ToggleField } from "decky-frontend-lib";
 import { useMemo, useState, useEffect } from "react";
 import { useCssLoaderState } from "../../state";
 import { storeWrite } from "../../python";
-import { disableNavPatch, enableNavPatch } from "../../deckyPatches/NavPatch";
+import { setNavPatch } from "../../deckyPatches/NavPatch";
 import {
   getWatchState,
   getServerState,
@@ -16,11 +16,6 @@ export function PluginSettings() {
   const [watchOn, setWatchOn] = useState<boolean>(false);
 
   const navPatchEnabled = useMemo(() => !!navPatchInstance, [navPatchInstance]);
-
-  function setNavPatch(value: boolean) {
-    value ? enableNavPatch() : disableNavPatch();
-    storeWrite("enableNavPatch", value + "");
-  }
 
   useEffect(() => {
     getServerState().then((res) => {
