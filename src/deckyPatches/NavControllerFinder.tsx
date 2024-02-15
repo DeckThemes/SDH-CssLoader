3aa18011af241c9959511afb7fc4de5b4944dbf8
Beebles <102569435+beebls@users.noreply.github.com>
2024-02-15 16:15:54 -0700

    fix nav patch on latest betas

--- src/deckyPatches/NavControllerFinder.tsx
+++ src/deckyPatches/NavControllerFinder.tsx
@@ -1,3 +1,18 @@
 import { Module, findModuleChild } from "decky-frontend-lib";
 
-export const NavController = findModuleChild((m: Module) => m?.CFocusNavNode);
+export const NavController = findModuleChild((m: Module) => {
+  if (typeof m !== "object") return undefined;
+
+  // Pre Chromium-109
+  if (m?.CFocusNavNode) {
+    return m.CFocusNavNode;
+  }
+
+  for (let prop in m) {
+    if (m[prop]?.prototype?.FindNextFocusableChildInDirection) {
+      return m[prop];
+    }
+  }
+
+  return undefined;
+});
