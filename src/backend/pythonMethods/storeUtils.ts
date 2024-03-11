2aa63dd52af0afcbe06aa0c2adb7f6f6d4ff7472
Beebles <102569435+beebls@users.noreply.github.com>
2024-03-11 12:39:35 -0600

    RAAAAAAAAAAAAAAAAAGH PART 22222

--- src/backend/pythonMethods/storeUtils.ts
+++ src/backend/pythonMethods/storeUtils.ts
@@ -14,7 +14,7 @@ export async function booleanStoreRead(key: string) {
 export async function booleanStoreWrite(key: string, value: boolean) {
   const deckyRes = await server!.callPluginMethod<{ key: string; val: string }>("store_write", {
     key,
-    value: val ? "1" : "0",
+    val: value ? "1" : "0",
   });
   if (!deckyRes.success) {
     toast(`Error fetching ${key}`, deckyRes.result);
